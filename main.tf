18940adc24362d47c23731886e1670164c37800e
Paul Bruffett <paulbruffett@protonmail.com>
2021-06-15 15:20:00 -0700

    deploying AML

--- main.tf
+++ main.tf
@@ -33,17 +33,46 @@ terraform {
     tenant_id       = var.tenant_id
 }
 
-     # An example resource that does nothing.
-    # Create a resource group
-    resource "azurerm_resource_group" "example" {
-    name     = "example-resources"
-    location = "West Europe"
-    }
 
-    # Create a virtual network within the resource group
-    resource "azurerm_virtual_network" "example" {
-    name                = "example-network"
-    resource_group_name = azurerm_resource_group.example.name
-    location            = azurerm_resource_group.example.location
-    address_space       = ["10.0.0.0/16"]
-    }
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "aml" {
+  name     = "azure-ml"
+  location = "West US 2"
+}
+
+resource "azurerm_application_insights" "aml" {
+  name                = "workspace-example-ai"
+  location            = azurerm_resource_group.aml.location
+  resource_group_name = azurerm_resource_group.aml.name
+  application_type    = "web"
+}
+
+resource "azurerm_key_vault" "aml" {
+  name                = "workspaceexamplekeyvault"
+  location            = azurerm_resource_group.aml.location
+  resource_group_name = azurerm_resource_group.aml.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+}
+
+resource "azurerm_storage_account" "aml" {
+  name                     = "workspacestorageaccount"
+  location                 = azurerm_resource_group.aml.location
+  resource_group_name      = azurerm_resource_group.aml.name
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_machine_learning_workspace" "aml" {
+  name                    = "tycho-workspace"
+  location                = azurerm_resource_group.aml.location
+  resource_group_name     = azurerm_resource_group.aml.name
+  application_insights_id = azurerm_application_insights.aml.id
+  key_vault_id            = azurerm_key_vault.aml.id
+  storage_account_id      = azurerm_storage_account.aml.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
