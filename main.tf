b9ab5692c9ded6dacd8f0cfcff874f143383ca0b
Paul Bruffett <paulbruffett@protonmail.com>
2021-06-15 22:19:32 -0700

    updated org

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
        backend "remote" {
          # The name of your Terraform Cloud organization.
-         organization = "example-organization"
+         organization = "pbazure"
 
          # The name of the Terraform Cloud workspace to store Terraform state files in.
          workspaces {
