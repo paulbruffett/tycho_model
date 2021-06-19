6bc9dbab0593db134f82f5b33472bd7fa7df0d9c
Paul Bruffett <paulbruffett@protonmail.com>
2021-06-19 08:23:05 -0700

    sequential workflow

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "azurerm_application_insights" "aml" {
 }
 
 resource "azurerm_key_vault" "aml" {
-  name                     = "tychoworkeyvault7ee732bc"
+  name                     = "tychoworkeyvault2"
   location                 = azurerm_resource_group.aml.location
   resource_group_name      = azurerm_resource_group.aml.name
   tenant_id                = data.azurerm_client_config.current.tenant_id
