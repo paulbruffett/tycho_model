41b0d081ab9839e825ac8789b9fd2124c95a0a18
Paul Bruffett <paulbruffett@protonmail.com>
2021-06-20 09:29:46 -0700

    admin enable acr

--- main.tf
+++ main.tf
@@ -82,6 +82,7 @@ resource "azurerm_container_registry" "aml" {
   location            = azurerm_resource_group.aml.location
   resource_group_name = azurerm_resource_group.aml.name
   sku                 = "Basic"
+  admin_enabled = true
 }
 
 
