75abf9b635259b871396205388a6ecc66f58583b
Paul Bruffett <paulbruffett@protonmail.com>
2021-06-30 20:28:49 -0700

    tensorboard logging

--- code/train/train.py
+++ code/train/train.py
@@ -4,8 +4,6 @@ from azureml.core import Workspace, Dataset
 from azureml.core import Run
 import ScrapePA
 import torch
-import mlflow.fastai
-import mlflow
 from functools import partial
 import pandas as pd
 import os
@@ -26,36 +24,42 @@ dls_lm = DataBlock(
     get_x=ColReader(0)).dataloaders(train, bs=64)
 dls_lm.show_batch(max_n=2)
 
+
 learn = language_model_learner(
     dls_lm, AWD_LSTM, drop_mult=0.3, 
     metrics=[accuracy, Perplexity()]).to_fp16()
 
-mlflow.fastai.autolog()
+tboard_path = Path('data/tensorboard/')
+learn.callback_fns.append(partial(LearnerTensorboardWriter, 
+                                    base_dir=tboard_path, 
+                                    name='base_run'))
 
-with mlflow.start_run():
-    learn.fit_one_cycle(1, 2e-2)
-    print("trained one model")
 
-    tycho_ds = Dataset.get_by_name(ws,"tycho_ds")
-    if not os.path.exists('data'):
-            os.makedirs('data')
-    tycho_ds.download("./data/",overwrite=True)
-    tycho = pd.read_json("data/"+os.listdir("data")[0])
-    tycho[0] = tycho[0].apply(lambda x: x.replace('\n', ''))
-    tycho[0] = tycho[0].apply(lambda x: x[145:])
+learn.fit_one_cycle(1, 2e-2)
+print("trained one model")
 
-    t_dls = DataBlock(
-        blocks=(TextBlock.from_df(0, is_lm=True, seq_len=72)),
-        get_x=ColReader(0)).dataloaders(tycho, bs=64)
-    t_dls.show_batch(max_n=2)
+tycho_ds = Dataset.get_by_name(ws,"tycho_ds")
+if not os.path.exists('data'):
+        os.makedirs('data')
+tycho_ds.download("./data/",overwrite=True)
+tycho = pd.read_json("data/"+os.listdir("data")[0])
+tycho[0] = tycho[0].apply(lambda x: x.replace('\n', ''))
+tycho[0] = tycho[0].apply(lambda x: x[145:])
 
-    learn = language_model_learner(
-        t_dls, AWD_LSTM, drop_mult=0.3, 
-        metrics=[accuracy, Perplexity()]).to_fp16()
+t_dls = DataBlock(
+    blocks=(TextBlock.from_df(0, is_lm=True, seq_len=72)),
+    get_x=ColReader(0)).dataloaders(tycho, bs=64)
+t_dls.show_batch(max_n=2)
 
-    #learn = learn.load('wiki103')
-    learn.unfreeze()
-    learn.fit_one_cycle(10, 2e-3)
+learn = language_model_learner(
+    t_dls, AWD_LSTM, drop_mult=0.3, 
+    metrics=[accuracy, Perplexity()]).to_fp16()
 
-    learn.save('tycho_model')
-    print("trained tycho model")
+#learn = learn.load('wiki103')
+learn.unfreeze()
+learn.fit_one_cycle(10, 2e-3)
+
+learn.save('tycho_model')
+print("trained tycho model")
+
+run.l
